ed3624cf2a057c9b6006881eb7a60b583e76d5a1
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2024-03-27 18:09:15 +0545

    Refactor ExpenseLineChart.tsx to import expenses from data file

--- src/app/charts/ExpenseLineChart.tsx
+++ src/app/charts/ExpenseLineChart.tsx
@@ -1,60 +1,8 @@
 "use client";
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
-interface Expense {
-  month: string;
-  value: number;
-}
-const expenses: Expense[] = [
-  {
-    month: "January",
-    value: 100,
-  },
-  {
-    month: "February",
-    value: 160,
-  },
-  {
-    month: "March",
-    value: 12,
-  },
-  {
-    month: "April",
-    value: 780,
-  },
-  {
-    month: "May",
-    value: 73,
-  },
-  {
-    month: "June",
-    value: 45,
-  },
-  {
-    month: "July",
-    value: 90,
-  },
-  {
-    month: "August",
-    value: 33,
-  },
-  {
-    month: "September",
-    value: 35,
-  },
-  {
-    month: "October",
-    value: 28,
-  },
-  {
-    month: "November",
-    value: 65,
-  },
-  {
-    month: "December",
-    value: 82,
-  },
-];
+import { Expense } from "../data/type";
+import expenses from "../data/expenses.data";
 
 const width = 640;
 const height = 400;
